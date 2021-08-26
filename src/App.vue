ca492a294aa005491762addabb2931405a7d9438
simonproudfoot <simonproudfoot1980@gmail.com>
2021-08-26 11:22:56 +0100

    save to array

--- src/App.vue
+++ src/App.vue
@@ -7,15 +7,12 @@
                 <span v-for="c in colours" class="color" :class="c == color ? 'selected' : null" :style="'background-color:' + c" :key="c" @click="selectColor(c)"></span>
             </div>
             <div class="text-white">Brush width</div>
-            <div style="
-            display: flex;
-            flex-wrap: wrap;
-            align-items: center;
-            justify-content: center;
-          ">
+            <div style=" display: flex; flex-wrap: wrap; align-items: center;justify-content: center;">
                 <span v-for="b in brushWidths" class="brushWidths" :class="b == dynamicLineWidth ? 'selected' : null" :style="{ height: b + 'px', width: b + 'px' }" :key="b" @click="selectBrush(b)"></span>
             </div>
+            <button class=" saveButton" @click="save">Save</button>
         </div>
+        <!-- canvas -->
         <div class="row g-0">
             <div class="col-6 wrapper__col left" style="position: relative">
                 <img :src="require('@/assets/butterfly.svg')" class="template left" />
@@ -28,13 +25,28 @@
             </div>
         </div>
     </div>
+    <!--canvas -->
+    <div class="previous">
+        <p class="text-white mb-0">Images get added to array (locally)</p>
+
+        <div v-for="(butterfly, i) in butterFlys" :key="i">
+            <div class="butterfly">
+                <img :src="butterfly" class="butterfly--left">
+                <div class="butterfly--body">
+                </div>
+                <img :src="butterfly" class="butterfly--right">
+            </div>
+        </div>
+    </div>
 </div>
 </template>
+
 <script>
 export default {
     name: "App",
     data() {
         return {
+            butterFlys: [],
             colours: ["#fd8686", "#ffa77c", "#f9d62e", "#a0e169", "#04d2d4", "#000"],
             brushWidths: [10, 20, 30, 40],
             currentImage: null,
@@ -62,6 +74,14 @@ export default {
         },
     },
     methods: {
+        save() {
+            this.butterFlys.unshift(this.currentImage)
+            this.isActive = false
+            setTimeout(() => {
+                this.isActive = true
+                this.mirrorScreen()
+            }, 100);
+        },
         selectColor(color) {
             this.color = color;
         },
@@ -100,7 +120,86 @@ export default {
     },
 };
 </script>
+
 <style>
+.butterfly {
+    position: relative;
+    width: 180px;
+    height: 180px;
+
+}
+
+.butterfly--body {
+    width: 15%;
+    height: 70px;
+    background-color: #000;
+    display: inline-block;
+    position: absolute;
+    top: 0;
+    left: 0;
+    margin: auto;
+    right: 0;
+    bottom: 0;
+    border-radius: 34px;
+}
+
+.butterfly--left {
+    width: 40%;
+    height: 70%;
+    background-color: #000;
+    display: inline-block;
+    position: absolute;
+    top: 0;
+    left: 2%;
+    margin: auto;
+    bottom: 0;
+    animation: spinLeft 1s linear infinite;
+    transform-origin: center right;
+}
+
+.butterfly--right {
+    width: 40%;
+    height: 70%;
+    background-color: #000;
+    display: inline-block;
+    position: absolute;
+    top: 0;
+    right: 2%;
+    margin: auto;
+    transform-origin: center left;
+    transform: scaleX(-1);
+    bottom: 0;
+    animation: spin 1s linear infinite;
+}
+
+@keyframes spinLeft {
+    0% {
+        transform: rotateY(40deg) rotateZ(-5deg)
+    }
+
+    50% {
+        transform: rotateY(-40deg) rotateZ(5deg)
+    }
+
+    100% {
+        transform: rotateY(40deg) rotateZ(-5deg)
+    }
+}
+
+@keyframes spin {
+    0% {
+        transform: rotateY(-40deg) rotateZ(5deg)
+    }
+
+    50% {
+        transform: rotateY(40deg) rotateZ(-5deg)
+    }
+
+    100% {
+        transform: rotateY(-40deg) rotateZ(5deg)
+    }
+}
+
 #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
@@ -109,6 +208,7 @@ export default {
     color: #2c3e50;
     height: 100%;
 }
+
 .template {
     width: 500px;
     height: unset;
@@ -121,6 +221,7 @@ export default {
     top: 0;
     mix-blend-mode: color-burn;
 }
+
 .wrapper {
     width: 1200px;
     height: 800px;
@@ -131,56 +232,91 @@ export default {
     top: 0;
     bottom: 0;
 }
+
 .wrapper__col {
     position: relative;
 
     background-size: contain;
     background-repeat: no-repeat;
 }
+
 .left {
     right: 0;
 }
+
 .right {
     left: 0;
 }
+
 .canvas,
 .mirror {
     width: 100% !important;
     height: 100% !important;
 }
+
 .paint {
     width: 100% !important;
 }
+
 .paintable {
     width: 100% !important;
     position: relative !important;
 }
+
 .mirror {
     -webkit-transform: scaleX(-1);
     transform: scaleX(-1);
 }
+
 .menu {
-    top: 10px;
-    left: 10px;
+    top: 0;
+    left: 0;
     position: fixed;
     width: 200px;
-    height: 500px;
+    height: 100%;
     background: brown;
     z-index: 1000;
 }
+
+.previous {
+    top: 0;
+    right: 0;
+    position: fixed;
+    width: 200px;
+    padding: 10px;
+    height: 100%;
+    background: brown;
+    z-index: 1000;
+
+}
+
 .selected {
     border: 1px #fff solid;
 }
+
 .color {
     width: 33.33%;
     height: 80px;
     cursor: pointer;
 }
+
 .brushWidths {
     background-color: #000;
     margin: 10px;
     height: 80px;
     cursor: pointer;
     border-radius: 100px;
+}
+
+.saveButton {
+    position: absolute;
+    bottom: 0;
+    background-color: blue;
+    width: 100%;
+    height: 100px;
+    display: block;
+    left: 0;
+    color: #fff;
+    border: none;
 }
 </style>
