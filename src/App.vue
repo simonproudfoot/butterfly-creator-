47d19409062a60bff33ec23f1e1844a65f73ca5d
simonproudfoot <simonproudfoot1980@gmail.com>
2022-01-13 23:22:33 +0000

    switcher

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
 <div id="app">
-     <!-- <div class="key" style="display:">
+    <div class="key" style="display:">
         Current
-        <div v-for="(img, i) in butterFlys" :key="i" >
+        <div v-for="(img, i) in butterFlys" :key="i">
             <img :src="img.image" />
             <small>{{img.wing}}</small>
         </div>
 
-    </div>  -->
+    </div>
 
     <v-idle style="display: none" :duration="100" :loop="true" @idle="idle" />
 
@@ -132,7 +132,7 @@ export default {
         };
     },
     methods: {
-        begin(){
+        begin() {
 
         },
         idle() {
@@ -304,7 +304,7 @@ export default {
             console.log('go')
         }
     },
-   
+
     watch: {
         canvas(val) {
             var options = {
